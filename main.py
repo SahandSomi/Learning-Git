b121263e55de60398cebcc47f3926d72b6ba6b82
aimunk <samarth.patel@altaml.com>
2022-01-19 10:56:44 -0700

    feat: Add divide function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def multiply(num1,num2)
+def multiply(num1,num2):
     return num1*num2
+
+def divide(num1, num2):
+    """ Trying to add to number
+    Args:
+        num1 ([int]): [description]
+        num2 ([int]): [description]
+    Returns:
+        [type]: [description]
+    """
+    return num1/num2
