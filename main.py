096181f0e329c7cde9ca5e4dcca20f68aa5eb26a
Sahand <ssomi@ualberta.ca>
2022-01-19 10:42:28 -0700

    doc

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ def multiply(num1,num2)
     return num1*num2
 
 def add (num1, num2):
+    """ Trying to add to number
+
+    Args:
+        num1 ([int]): [description]
+        num2 ([int]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     return num1+num2
